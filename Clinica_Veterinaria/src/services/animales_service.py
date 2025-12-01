90c93517eb723bf6b9cbcdf65923f0032d55afa6
Daniel <9300411@alumnos.ufv.es>
2025-12-01 18:31:15 +0100

    Implementamos las funcionalidades para crear y listar los animales

--- Clinica_Veterinaria/src/services/animales_service.py
+++ Clinica_Veterinaria/src/services/animales_service.py
@@ -1,18 +1,26 @@
 # Aquí se programarán las funciones que gestionan la lógica de negocio de los animales.
 # Por ejemplo: crear, consultar, listar, actualizar y eliminar animales.
 
-class AnimalService:
-    def crear_animal(self):
-        pass
+from sqlalchemy.orm import Session
+from src.db.models import Animal
+from src.schemas.animal_schema import AnimalCreate
 
-    def obtener_animal(self):
-        pass
+class AnimalesService:
 
-    def listar_animales(self):
-        pass
+    @staticmethod
+    def crear_animal(db: Session, animal_data: AnimalCreate):
+        nuevo_animal = Animal(**animal_data.model_dump())
+        db.add(nuevo_animal)
+        db.commit()
+        db.refresh(nuevo_animal)
+        return nuevo_animal
 
-    def actualizar_animal(self):
-        pass
+    @staticmethod
+    def listar_animales(db: Session):
+        return db.query(Animal).all()
 
-    def eliminar_animal(self):
-        pass
+    @staticmethod
+    def obtener_animal(db: Session, animal_id: int):
+        return db.query(Animal).filter(Animal.id == animal_id).first()
+    
+    # (PENDIENTE: Agregar lógica de Actualizar y Eliminar en pasos posteriores para mantenerlo simple)
