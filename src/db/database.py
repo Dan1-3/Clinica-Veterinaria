9d7c3d152f57973bf2a56984bc51e81d58f0e22e
Daniel <9300411@alumnos.ufv.es>
2025-12-04 09:35:38 +0100

    Prueba para comprobar funcionamiento github

--- src/db/database.py
+++ src/db/database.py
@@ -1,23 +1,23 @@
 # Aquí se establecerá la conexión con la base de datos (SQLite).
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine  # Importamos create_engine para crear la conexión con la base de datos
+from sqlalchemy.ext.declarative import declarative_base #Importamos declarative_base para definir la clase base de los modelos
+from sqlalchemy.orm import sessionmaker # Importamos sessionmaker para crear sesiones de base de datos
 
-# 1. Configuración de SQLite
+# 1. Configuración de SQLite, la URL de la base de datos
 SQLALCHEMY_DATABASE_URL = "sqlite:///./clinica.db"
 
-# 2. Creamos el motor de la base de datos
+# 2. Creamos el motor de la base de datos, para conectarnos a SQLite
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
 
-# 3. Creamos la sesión (para poder guardar/leer datos)
+# 3. Creamos la sesión (para poder guardar/leer datos), vinculada al motor
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-# 4. Clase Base para los modelos
+# 4. Clase Base para los modelos, que heredan de ella
 Base = declarative_base()
 
-# 5. ESTA ES LA FUNCIÓN QUE TE FALTA (get_db)
+# 5. Dependencia para obtener la sesión de la base de datos, usada en FastAPI
 def get_db():
     db = SessionLocal()
     try:
