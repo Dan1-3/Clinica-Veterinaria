2ee9fb0c7f41a6edbdaa40581c4f8d053c0c5be7
Santirramallo <santiagoramallo05@gmail.com>
2025-12-04 09:04:18 +0100

    Borramos un archivo duplicado donde no se nos estaban guardando los cambios y nos reoorganizamos en estas carpetas que tienen todo nuestro contenido al día

--- Clinica_Veterinaria/tests/test_animales.py
+++
@@ -1,8 +0,0 @@
-# Aquí se escribirán las pruebas unitarias para las funciones de animales.
-# Servirán para comprobar que las operaciones CRUD funcionan correctamente.
-
-def test_crear_animal():
-    pass
-
-def test_listar_animales():
-    pass
